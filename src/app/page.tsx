283d0b84697dec8067c904beac3704d7ffa74db6
Micci <kh.misha.9@icloud.com>
2024-10-12 01:30:27 +0300

    main page v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import Component from "@/components/component";
+import { MainPage } from "@/components/main-page";
 
 export default function Home() {
-  return <Component />
+  return <MainPage />
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
